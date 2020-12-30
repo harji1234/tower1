ec2312414a2bfb6a4be8599e1fceeecc76fb189a
harji1234 <73263036+harji1234@users.noreply.github.com>
2020-12-29 19:06:16 -0500

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,28 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill("blue")
+        stroke("black")
+        strokeWeight(3)
+        rect(0,0,this.width, this.height);
+        pop();
+      }
+}
